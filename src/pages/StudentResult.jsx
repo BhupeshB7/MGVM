23a815a61fbb130c940f32f356b6e86b1cc718a0
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-03-22 18:35:26 +0530

    update

--- src/pages/StudentResult.jsx
+++ src/pages/StudentResult.jsx
@@ -12,18 +12,14 @@ const LoadingSpinner = () => (
     </Spinner>
   </div>
 );
-const StudentResult = ({token}) => {
+const StudentResult = () => {
   const navigate = useNavigate();
   const [students, setStudents] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  useEffect(()=>{
-    if(!token){
-      navigate('/login');
-    }
-  },[])
+ 
   useEffect(() => {
     const fetchStudents = async () => {
       setLoading(true);
